edfaac62e54bb3e1e90f7d2b184dfc54bbfe37a5
edoski <87473803+edoski@users.noreply.github.com>
2023-11-04 13:14:02 +0100

    (CREATED NEW LOGIN-AREA BRANCH) 1. fixed trials int count bug 2. implemented security shutdown after 3 trials 3. linked AdminScene.fxml to LoginScene.fxml after successful login

--- src/main/java/login_menu/AdminController.java
+++ src/main/java/login_menu/AdminController.java
@@ -1,11 +1,14 @@
 package login_menu;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminController {
     @FXML
@@ -23,9 +26,13 @@ public class AdminController {
     private final String password = "onward";
     private int trials = 0;
 
-    public void loginAdmin() {
+    public void loginAdmin() throws IOException {
         if (adminUsername.getText().equals(username) && adminPassword.getText().equals(password)) {
-            // switch to loginscene
+            Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
         } else {
             String errorCase;
             if (adminUsername.getText().equals(username))
@@ -38,19 +45,24 @@ public class AdminController {
             checkLoginLabel.setText("Error, incorrect " + errorCase);
             checkLoginLabel.setTextFill(Color.RED);
             checkLoginLabel.setStyle("-fx-font-weight: bold;");
-//            set to bold checkLoginLabel
 
-
-//            trials aumenta di 2 ogni click di loginButton
-
-
-            trials++;
-            trialsLabel.setText("Attempt: " + trials);
+            trialsLabel.setText("Attempt: " + ++trials);
             trialsLabel.setTextFill(Color.RED);
             trialsLabel.setStyle("-fx-font-weight: bold;");
 
-            //if (trials == 3)
-        }
+            if (trials == 3) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning");
+                alert.setHeaderText("You have exceeded the number of trials");
+                alert.setContentText("If you do not login with the correct credentials the next attempt, the program will shut down for safety reasons.");
+                alert.showAndWait();
 
+            }
+
+            if (trials > 3) {
+                Stage stage = (Stage) loginButton.getScene().getWindow();
+                stage.close();
+            }
+        }
     }
 }
