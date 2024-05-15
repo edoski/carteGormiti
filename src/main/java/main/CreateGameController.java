895e4854fe81bf613a9472ba175cd579da67cf54
edoski <87473803+edoski@users.noreply.github.com>
2024-05-15 23:36:41 +0200

    Polished functionality for CreateGameController

--- src/main/java/main/CreateGameController.java
+++ src/main/java/main/CreateGameController.java
@@ -25,6 +25,7 @@ public class CreateGameController implements Initializable {
 
 	@FXML
 	private Label codeLabel;
+	private String code;
 	// HashSet per memorizzare i codici generati
 	private final HashSet<String> activeCodes = new HashSet<>();
 
@@ -56,7 +57,6 @@ public class CreateGameController implements Initializable {
 		players = new Player[4];
 
 		// TODO: GET CODES FROM FILESYSTEM AND LOAD THEM ONTO THE HASHSET
-
 		generateCode();
 	}
 
@@ -86,8 +86,40 @@ public class CreateGameController implements Initializable {
 		 * Se l'inserimento del robot corrisponde al terzo giocatore, viene lanciato un alert (vedi commenti su addHuman() al riguardo)
 		 */
 
-		Player player = new Player("CPU " + (plyrIndex + 1));
-		players[plyrIndex++] = player;
+		if (plyrIndex < 4) {
+			int CPUCount = 1;
+			// TODO: CHECK IF CPU BASED ON OBJECT TYPE, AND NOT ON NAME
+			for (Player player : players) {
+				if (player != null && player.getName().contains("CPU")) {
+					CPUCount++;
+				}
+			}
+
+			Player player = new Player("CPU 0" + CPUCount);
+
+			switch (plyrIndex) {
+				case 0:
+					player1.setText("Player 1: " + player.getName());
+					break;
+				case 1:
+					player2.setText("Player 2: " + player.getName());
+					break;
+				case 2:
+					player3.setText("Player 3: " + player.getName());
+					break;
+				case 3:
+					player4.setText("Player 4: " + player.getName());
+					break;
+			}
+
+			players[plyrIndex++] = player;
+		} else {
+			// Non puoi aggiungere più di 4 giocatori
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.setTitle("Warning");
+			alert.setHeaderText("Non puoi aggiungere più di 4 giocatori");
+			alert.showAndWait();
+		}
 	}
 
 	@FXML
@@ -101,6 +133,17 @@ public class CreateGameController implements Initializable {
 
 		// Get the text from the text field
 		String playerName = playerNameTextField.getText();
+
+		// Check if the name is already taken
+		for (Player player : players) {
+			if (player != null && player.getName().equals(playerName)) {
+				Alert alert = new Alert(Alert.AlertType.WARNING);
+				alert.setTitle("Warning");
+				alert.setHeaderText("Nome già in uso");
+				alert.showAndWait();
+				return;
+			}
+		}
 
 		// Check if the text field is empty
 		if (playerName.isEmpty()) {
@@ -130,6 +173,8 @@ public class CreateGameController implements Initializable {
 
 				Player player = new Player(playerName);
 				players[plyrIndex++] = player;
+
+				playerNameTextField.clear();
 			} else {
 				// Non puoi aggiungere più di 4 giocatori
 				Alert alert = new Alert(Alert.AlertType.WARNING);
@@ -149,7 +194,7 @@ public class CreateGameController implements Initializable {
 		 */
 
 		Random rand = new Random();
-		String code = "";
+		code = "";
 		for (int i = 0; i < 4; i++) {
 			if (i < 2) {
 //				Generiamo due caratteri maiuscoli in A-Z per la prima parte del codice
@@ -164,7 +209,6 @@ public class CreateGameController implements Initializable {
 		if (activeCodes.contains(code)) {
 			generateCode();
 		} else {
-			activeCodes.add(code);
 			codeLabel.setText("Code: " + code);
 		}
 	}
@@ -205,5 +249,70 @@ public class CreateGameController implements Initializable {
 		 *      - Arrotondato a 4 se sono aggiunti 3 giocatori/robot
 		 *
 		 */
+
+		// Tally player count
+		int count = 0;
+		for (Player player : players) {
+			if (player != null) {
+				count++;
+			}
+		}
+
+		switch (count) {
+			case 0:
+				// Non ci sono giocatori
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setTitle("Warning");
+				alert.setHeaderText("Non ci sono giocatori");
+				alert.setContentText("Aggiungi almeno un giocatore per iniziare la partita");
+				alert.showAndWait();
+				break;
+			case 1:
+				// Partita singola
+				Alert alert1 = new Alert(Alert.AlertType.CONFIRMATION);
+				alert1.setTitle("Partita singola");
+				alert1.setHeaderText("Sei sicuro di voler iniziare una partita singola?");
+				alert1.setContentText("Confermando, verrà aggiunto un giocatore CPU per completare la partita");
+				alert1.showAndWait();
+				// TODO: Add a CPU player
+				Player player2 = new Player("CPU 1");
+				players[1] = player2;
+				activeCodes.add(code);
+				// TODO: Start game
+				break;
+			case 2:
+				// Partita singola
+				Alert alert2 = new Alert(Alert.AlertType.CONFIRMATION);
+				alert2.setTitle("Partita singola");
+				alert2.setHeaderText("Sei sicuro di voler iniziare una partita singola?");
+				alert2.setContentText("Confermando, comincerà la partita con i due giocatori inseriti");
+				alert2.showAndWait();
+				activeCodes.add(code);
+				// TODO: Start game
+				break;
+			case 3:
+				// Torneo
+				Alert alert3 = new Alert(Alert.AlertType.CONFIRMATION);
+				alert3.setTitle("Torneo");
+				alert3.setHeaderText("Sei sicuro di voler iniziare un torneo?");
+				alert3.setContentText("Confermando, verrà aggiunto un giocatore CPU per completare il torneo");
+				alert3.showAndWait();
+				// TODO: Add a CPU player
+				Player player3 = new Player("CPU 1");
+				players[2] = player3;
+				activeCodes.add(code);
+				// TODO: Start game
+				break;
+			case 4:
+				// Torneo
+				Alert alert4 = new Alert(Alert.AlertType.CONFIRMATION);
+				alert4.setTitle("Torneo");
+				alert4.setHeaderText("Sei sicuro di voler iniziare un torneo?");
+				alert4.setContentText("Confermando, comincerà il torneo con i quattro giocatori inseriti");
+				alert4.showAndWait();
+				activeCodes.add(code);
+				// TODO: Start game
+				break;
+		}
 	}
 }
